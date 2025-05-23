5df6b80b7d61b034b59153da03356cda0c423924
Ill-tableflip-U <dylan.harvey612@gmail.com>
2025-05-23 16:56:17 +1000

    add semicolons

--- components/jsdrive/jsdrive.cpp
+++ components/jsdrive/jsdrive.cpp
@@ -42,7 +42,7 @@ static int segs_to_num(uint8_t segments) {
    case 0x6f:
     return 9;
    case 0x79:
-    return -2
+    return -2;
    default:
     ESP_LOGV(TAG, "unknown digit: %02f", segments & 0x7f);
   }
@@ -89,7 +89,7 @@ void JSDrive::loop() {
         int d2 = segs_to_num(d[2]);
         if (d0 == -2)
           ESP_LOGE(TAG, "Desk error code %d%d%d", d0, d1, d2)
-          break
+          break;
         if (d0 < 0 || d1 < 0 || d2 < 0)
           break;
         num = segs_to_num(d[0]) * 100 + segs_to_num(d[1]) * 10 + segs_to_num(d[2]);
