13dd0217b0767498c0ae1fdd72c44a3e4ab58e3b
Ill-tableflip-U <125164221+Ill-tableflip-U@users.noreply.github.com>
2025-06-28 04:09:38 +1000

    Update jsdrivev2.cpp

--- components/jsdrivev2/jsdrivev2.cpp
+++ components/jsdrivev2/jsdrivev2.cpp
@@ -163,7 +163,7 @@ void Desktronic::read_remote_uart()
         // Check if the received byte is 0xA5 and respond with the desired 5-byte command
         //if (byte == 0xA5)
         //{
-            uint8_t response[5] = {0x5A, 0x00, 0x00, 0x00, 0x00};
+            uint8_t response[5] = {0x5A, 0x06, 0x00, 0x00, 0x00};
             remote_uart_->write_array(response, 5); // Send the response
             ESP_LOGI(TAG, "Received 0xA5, responding with: 0x5A 0x00 0x00 0x00 0x00");
        // }
